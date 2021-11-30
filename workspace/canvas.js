c3a003d9a33130bb8068722c9c0abbe5ba718fca
dev_tree <71920791+jaeyunlim@users.noreply.github.com>
2021-11-30 15:43:10 +0900

    Update canvas.js

--- workspace/canvas.js
+++ workspace/canvas.js
@@ -7,6 +7,8 @@ const fillOrPaint = document.querySelector(".jsMode button:first-child");
 
 const color = document.querySelectorAll(".color");
 
+const saveBtn = document.querySelector(".jsMode button:last-child");
+
 canvas.width = 700;
 canvas.height = 700;
 
@@ -14,6 +16,9 @@ ctx.strokeStyle = "#212529";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+
+ctx.fillStyle = "#e5e5e5"
+ctx.fillRect(0,0,canvas.width,canvas.height);
 
 const colorArr = Array.from(color);
 
@@ -23,19 +28,20 @@ colorArr.forEach(function (item) {
 
 function changeColor(event) {
     const clickedColor = event.target.style.backgroundColor;
-    if(fillOrPaint.innerHTML === "PAINT"){
+    if(fillOrPaint.innerHTML === "COLOR"){
         console.log("i will paint");
         ctx.strokeStyle = clickedColor;
     } else if(fillOrPaint.innerHTML === "FILL") {
         console.log("i will fill");
-        canvas.style.backgroundColor = clickedColor;
+        ctx.fillStyle = clickedColor;
+        ctx.fillRect(0,0,canvas.width,canvas.height);
     }
-
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+
     if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -75,11 +81,20 @@ function setMode() {
     }
 }
 
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "MYCANVAS🎨";
+    link.click();
+}
+
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseLeave);
 canvas.addEventListener("contextmenu", controlContextMenu);
+saveBtn.addEventListener("click", handleSaveClick);
 
 range.addEventListener("input", handleRange);
 
